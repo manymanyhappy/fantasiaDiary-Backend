e4ba4bb2aecbf1866d73d16a8118831b6c495701
DAHEE <dahee@DAHEEui-MacBookPro.local>
2020-12-18 22:52:44 +0900

    refactor: modify FantasiaDiary Schema

--- models/FantasiaDiary.js
+++ models/FantasiaDiary.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const fantasiaDiarySchema = new mongoose.Schema({
   creator: {
     type: String,
-    unique: true,
     required: true,
     trim: true
   },
